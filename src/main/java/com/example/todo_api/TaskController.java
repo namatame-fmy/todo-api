b257efebe65855fde946137fe0c9b2c0264e7a34
fumiya-namatame <mariypuuu0519@gmail.com>
2026-07-24 06:53:17 +0900

    Vue接続のため@CrossOriginでCORSを許可

--- src/main/java/com/example/todo_api/TaskController.java
+++ src/main/java/com/example/todo_api/TaskController.java
@@ -2,13 +2,22 @@ package com.example.todo_api;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import  org.springframework.web.bind.annotation.PutMapping;
+
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class TaskController {
     private List<Task> tasks = new ArrayList<>();
 
@@ -26,5 +35,19 @@ public class TaskController {
     public  Task addTask(@RequestBody Task newTask){
         tasks.add(newTask);
         return newTask;
+    }
+
+    @DeleteMapping("/tasks/{index}")
+    public void deleteTask(@PathVariable int index) {
+        if(index >= 0 && index < tasks.size()) {
+            tasks.remove(index);
+        }
+    }
+
+    @PutMapping("/tasks/{index}")
+    public void completeTask(@PathVariable int index) {
+        if (index >= 0 && index < tasks.size()){
+            tasks.get(index).setDone(true);
+        }
     }
 }
